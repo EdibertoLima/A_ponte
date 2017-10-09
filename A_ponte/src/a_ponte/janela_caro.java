edc6d78c4fcc8cbb0430d58c19785c85b9c35932
ediberto <ediberto45@gmail.com>
2017-10-09 14:41:11 -0300

    fiz

--- A_ponte/src/a_ponte/janela_caro.java
+++ A_ponte/src/a_ponte/janela_caro.java
@@ -33,7 +33,7 @@ public class janela_caro extends javax.swing.JFrame {
         x.start();
         
     }
-
+// fdffdfdfd
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
